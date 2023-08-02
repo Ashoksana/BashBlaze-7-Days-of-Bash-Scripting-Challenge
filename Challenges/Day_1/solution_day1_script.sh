c8f1175c4ed9947c699b967a71732626a705fae9
Ashok Sana <107429355+Ashoksana@users.noreply.github.com>
2023-08-02 07:31:28 +0530

    Update Bashscript01.sh

--- Challenges/Day_1/solution_day1_script.sh
+++ Challenges/Day_1/solution_day1_script.sh
@@ -1,31 +1,77 @@
 #!/bin/bash
 
-# First line of the script is the shebang which tells the system how to execute
+#Author: Ashok_Sana
 
-# Task  2: Echo
-echo "Scripting is fun with @TWS"
+#Task 1: comment
+echo "Hello friends, good morning!" # This is single line comment.
 
-# Task  3: Variables
-variable1="Hello"
-variable2="Bash"
+#for multiline comments in shell script
+<<COMMENT
+This is a multiple line of comment in a shell script.
+You can use multiple lines like this in a shell script.
+it acts as a block comment and won't be executed.
+COMMENT
 
-# Task  4: Using Variables
-greeting="$variable1, $variable2!"
-echo "$greeting Welcome to the world of Bash scripting!"
+echo "==============================================================================================================================================="
+#Task_2: Variables
+#Declaaring the athematic variable in a shell script
+num1=10
+num2=14
+sum=$((num1 + num2))
 
-# Task  5: Using Built-in Variables
-echo "My current bash path - $BASH"
-echo "Bash version I am using - $BASH_VERSION"
-echo "PID of bash I am running - $$"
-echo "My home directory - $HOME"
-echo "Where am I currently? - $PWD"
-echo "My hostname - $HOSTNAME"
+echo "The sum of $num1 and $num2 is: $sum"
 
-# Task  6: Wildcards
-echo "Files with .txt extension in the current directory:"
-ls *.txt
+#Task_2.1
+
+Name="Ashok"
+Age=26
+spell="EXPLOREWITHASHOK"
+
+#Displaying the variables.
+
+echo "My name is $Name and my age is $Age yrs. My spell is $spell"
+
+# This script takes two numbers as input and prints their sum.
+
+# Taking user input for two numbers
+read -p "Enter the first number: " num1
+read -p "Enter the second number: " num2
+
+# Calculating the sum
+sum=$((num1 + num2))
+
+# Displaying the result
+echo "The sum of $num1 and $num2 is: $sum"
+echo "============================================================================================================================================="
+
+#This script uses the echo command to display a message
+#Task_3:echo
+echo "Welcome to my class"
+
+#If you want to print multiple-lines using echo
+echo -e "This 1st line. \nThis is the second line. \nThis 3rd line."
+
+echo "============================================================================================================================================="
+
+#Task_4: Using built-in Variables
 
 
+# This script demonstrates the use of built-in variables.
 
-#Make sure to provide execution permission with the following command:
-#chmod +x day1_script.sh
+# Displaying the current date and time
+echo "Current date and time: $(date)"
+
+# Displaying the username of the current user
+echo "Username: $USER"
+
+# Displaying the hostname of the machine
+echo "Hostname: $(hostname)"
+echo "============================================================================================================================================="
+# This script lists all files with a specific extension in a directory.
+
+# Specify the directory path
+directory="/home/sana/dockerfolder"
+
+# List all files with .yaml extension in the directory
+echo "Files with .yaml extension in the directory:"
+ls $directory/*.yaml
